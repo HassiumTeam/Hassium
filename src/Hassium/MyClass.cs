4a7d2b901ce57c5b54986a925785c3d4c4cc91d7
Jacob Misirian <misirianrule@gmail.com>
2015-08-20 14:16:16 -0500

    Added command line arguments

--- src/Hassium/MyClass.cs
+++ src/Hassium/MyClass.cs
@@ -17,15 +17,39 @@ namespace Hassium
             }
         }
 
+        private static class options
+        {
+            public static bool Debug = false;
+            public static string FilePath = "";
+        }
+
         public static void Main(string[] args)
         {
+            if (args[0].StartsWith("-d") || args[0].StartsWith("--debug"))
+            {
+                options.Debug = true;
+                options.FilePath = args[1];
+                Interpreter.variables.Add("args", shiftArray(args, 2));
+            }
+            else if (args[0].StartsWith("-h") || args[0].StartsWith("--help"))
+            {
+                Console.WriteLine("USAGE: Hassium.exe [OPTIONS] [FILE] [ARGUMENTS]\nArguments:\n-h  --help\tShows this help\n-d  --debug\tDisplays tokens from lexer\n");
+                Environment.Exit(0);
+            }
+            else
+            {
+                options.FilePath = args[0];
+                Interpreter.variables.Add("args", shiftArray(args, 1));
+            }
+
             Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture; // zdimension: without that, decimal numbers doesn't work on other cultures (in france and other countries we use , instead of . for floating-point number)
-            Interpreter.variables.Add("args", shiftArray(args, 1));
+
             Interpreter.variables.Add("true", true);
             Interpreter.variables.Add("false", false);
 
-            List<Token> tokens = new Lexer(File.ReadAllText(args[0])).Tokenize();
-            //Debug.PrintTokens(tokens);
+            List<Token> tokens = new Lexer(File.ReadAllText(options.FilePath)).Tokenize();
+            if (options.Debug)
+                Debug.PrintTokens(tokens);
             Parser hassiumParser = new Parser(tokens);
             AstNode ast = hassiumParser.Parse();
             new Interpreter(ast).Execute();
