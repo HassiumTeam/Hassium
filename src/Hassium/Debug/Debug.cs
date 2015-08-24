2a3591758d376d87ce887f6b6f7dd18d4b12714a
zdimension <zippedfire@free.fr>
2015-08-24 17:11:02 +0200

    Fixed the spaces in the Debug.PrintTokens method

--- src/Hassium/Debug/Debug.cs
+++ src/Hassium/Debug/Debug.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Hassium
 {
@@ -7,10 +8,17 @@ namespace Hassium
     {
         public static void PrintTokens(List<Token> tokens)
         {
+            var position = 0;
             foreach (Token token in tokens)
             {
-                Console.WriteLine("Type: " + token.TokenClass.ToString() + "\tValue: " + token.Value);
+                Console.WriteLine(position + new string(' ', tokens.Count.ToString().Length + 2 - position.ToString().Length) + "Type: " + token.TokenClass +
+                                  new string(' ',
+                                      tokens.Max(x => x.TokenClass.ToString().Length) + 3 -
+                                      token.TokenClass.ToString().Length) + "Value: " + token.Value);
+                position++;
             }
+            Console.WriteLine("End of token list");
+            Console.WriteLine();
         }
     }
 }
