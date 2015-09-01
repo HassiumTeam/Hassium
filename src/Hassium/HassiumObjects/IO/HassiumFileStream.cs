c10e0609d949505c37b73269d548e2805b8cbb59
zdimension <zippedfire@free.fr>
2015-09-01 20:58:42 +0200

    Pushed file that has been ignored (dunno why)

---
+++ src/Hassium/HassiumObjects/IO/HassiumFileStream.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Hassium.HassiumObjects.IO
+{
+    public class HassiumFileStream: HassiumStream
+    {
+        public new FileStream Value
+        {
+            get { return (FileStream)base.Value; }
+            set { base.Value = value; }
+        }
+
+        public HassiumFileStream(FileStream value) : base(value)
+        {
+            Attributes.Add("readChar", new InternalFunction(ReadChar));
+        }
+
+        public HassiumObject ReadChar(HassiumObject[] args)
+        {
+            return ((char) Value.ReadByte()).ToString();
+        }
+    }
+}
+
