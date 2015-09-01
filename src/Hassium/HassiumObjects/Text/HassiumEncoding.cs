aa24ff0a8b775b88d22fc608ac35303271517246
Jacob Misirian <misirianrule@gmail.com>
2015-09-01 16:59:28 -0500

    Began encoding

---
+++ src/Hassium/HassiumObjects/Text/HassiumEncoding.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using Hassium.HassiumObjects;
+using Hassium.HassiumObjects.Types;
+
+namespace Hassium
+{
+    public class HassiumEncoding: HassiumObject
+    {
+        public Encoding Value { get; private set; }
+
+        public HassiumEncoding(HassiumString type)
+        {
+            switch (type.Value)
+            {
+                case "ASCII":
+                    this.Value = Encoding.ASCII;
+                    break;
+                case "UTF8":
+                    this.Value = Encoding.UTF8;
+                    break;
+                case "UTF7":
+                    this.Value = Encoding.UTF7;
+                    break;
+                case "UTF32":
+                    this.Value = Encoding.UTF32;
+                    break;
+                default:
+                    this.Value = Encoding.ASCII;
+                    break;
+            }
+            this.Attributes.Add("bodyName", new InternalFunction(bodyName));
+            this.Attributes.Add("headerName", new InternalFunction(headerName));
+
+        }
+
+        private HassiumObject bodyName(HassiumObject[] args)
+        {
+            return new HassiumString(this.Value.BodyName);
+        }
+
+        private HassiumObject headerName(HassiumObject[] args)
+        {
+            return new HassiumString(this.Value.HeaderName);
+        }
+    }
+}
+
