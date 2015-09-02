0040c2899af78507c13864105e3449a3adc43707
zdimension <zippedfire@free.fr>
2015-09-02 17:15:46 +0200

    included

---
+++ src/Hassium/Interpreter/ParseException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Hassium.Parser;
+
+namespace Hassium.Interpreter
+{
+    public class ParseException : Exception
+    {
+        public AstNode Node { get; private set; }
+        public int Position { get; private set; }
+
+
+        public ParseException(string message, AstNode node) : this(message, node.Position)
+        {
+            Node = node;
+        }
+
+        public ParseException(string message, int position) : base(message)
+        {
+            Position = position;
+        }
+    }
+}
