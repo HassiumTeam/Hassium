8afba9f3fa3d5bd873c80757bf4594b37b9a4e0f
zdimension <zippedfire@free.fr>
2015-09-09 21:20:33 +0200

    Fixed MySQL

--- src/Hassium/HassiumObjects/MySql/HassiumSql.cs
+++ src/Hassium/HassiumObjects/MySql/HassiumSql.cs
@@ -15,6 +15,7 @@ namespace Hassium.HassiumObjects.Sql
         {
             Value = new MySqlConnection("Server=" + server + ";Database=" + database + ";User ID=" + user + ";Password=" + password + ";Pooling=false");
             Attributes.Add("open", new InternalFunction(open));
+            Attributes.Add("close", new InternalFunction(close));
             Attributes.Add("query", new InternalFunction(query));
             Attributes.Add("select", new InternalFunction(select));
         }
