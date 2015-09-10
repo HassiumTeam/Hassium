4ca62cac1ab4bf14c30b96e13dc98f9405a30d9f
Jacob Misirian <misirianrule@gmail.com>
2015-09-10 10:50:52 -0500

    Finished HassiumDataReader type

--- src/Hassium/HassiumObjects/MySql/HassiumSqlDataReader.cs
+++ src/Hassium/HassiumObjects/MySql/HassiumSqlDataReader.cs
@@ -16,6 +16,10 @@ namespace Hassium.HassiumObjects.Sql
             Value = value;
             Attributes.Add("read", new InternalFunction(read));
             Attributes.Add("get", new InternalFunction(get));
+            Attributes.Add("depth", new InternalFunction(depth));
+            Attributes.Add("close", new InternalFunction(close));
+            Attributes.Add("dispose", new InternalFunction(dispose));
+            Attributes.Add("getString", new InternalFunction(getString));
         }
 
         private HassiumObject read(HassiumObject[] args)
@@ -26,6 +30,37 @@ namespace Hassium.HassiumObjects.Sql
         private HassiumObject get(HassiumObject[] args)
         {
             return new HassiumString(Value[args[0].ToString()].ToString());
+        }
+
+        private HassiumObject depth(HassiumObject[] args)
+        {
+            return new HassiumDouble(Value.Depth);
+        }
+
+        private HassiumObject close(HassiumObject[] args)
+        {
+            Value.Close();
+
+            return null;
+        }
+
+        private HassiumObject dispose(HassiumObject[] args)
+        {
+            Value.Dispose();
+
+            return null;
+        }
+
+        private HassiumObject getString(HassiumObject[] args)
+        {
+            if (args[0] is HassiumString)
+                return new HassiumString(Value.GetString(args[0].ToString()));
+            else if (args[0] is HassiumDouble)
+                return new HassiumString(Value.GetString(((HassiumDouble)args[0]).ValueInt));
+            else if (args[0] is HassiumInt)
+                return new HassiumString(Value.GetString(((HassiumInt)args[0]).Value));
+            else
+                throw new Exception("Argument to getString() was not the correct format");
         }
     }
 }
