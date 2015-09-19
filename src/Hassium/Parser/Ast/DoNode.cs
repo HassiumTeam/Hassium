ecd5e761313987edbf52a84ad36f16b86bc93d2d
Jacob Misirian <misirianrule@gmail.com>
2015-09-19 18:17:46 -0500

    Added do loop.

---
+++ src/Hassium/Parser/Ast/DoNode.cs
@@ -0,0 +1,29 @@
+﻿using Hassium.Interpreter;
+
+namespace Hassium.Parser.Ast
+{
+    public class DoNode : AstNode
+    {
+        public AstNode Predicate
+        {
+            get { return Children[0]; }
+        }
+
+        public AstNode DoBody
+        {
+            get { return Children[1]; }
+        }
+
+        public DoNode(int position, AstNode predicate, AstNode doBody) : base(position)
+        {
+            Children.Add(predicate);
+            Children.Add(doBody);
+        }
+
+        public override object Visit(IVisitor visitor)
+        {
+            return visitor.Accept(this);
+        }
+    }
+}
+
