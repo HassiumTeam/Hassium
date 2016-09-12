f0f2ba989f5413fb5af2610a398647bd7992c80a
Jacob Misirian <jacob@localhost.localdomain>
2016-09-12 11:39:27 -0500

    Started work on TypeConverter class.

---
+++ src/Hassium/Runtime/Objects/Types/HassiumTypeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hassium.Runtime.Objects.Types
+{
+	public class HassiumTypeConverter: HassiumObject
+	{
+		public static new HassiumTypeDefinition TypeDefinition = new HassiumTypeDefinition("TypeConverter");
+
+		public HassiumTypeConverter()
+		{
+			AddType(TypeDefinition);
+		}
+
+		public HassiumList getBytes(VirtualMachine vm, params HassiumObject[] args)
+		{
+			HassiumList result = new HassiumList(new HassiumObject[0]);
+			byte[] bytes;
+			return result;
+		}
+	}
+}
