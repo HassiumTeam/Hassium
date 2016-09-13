a64e8ae6eff6fd1badb1c51b01bf3001f81a89f3
Jacob Misirian <misirianrule@gmail.com>
2016-09-12 22:32:14 -0500

    Updated AssemblyInfo.cs

--- src/Hassium/Properties/AssemblyInfo.cs
+++ src/Hassium/Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Hassium")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Hassium Language Core Interpreter")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HassiumTeam")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("reagan")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("HassiumTeam-(Jacob Misirian and Tom Niget")]
+[assembly: AssemblyTrademark("2015-2016")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.3.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
