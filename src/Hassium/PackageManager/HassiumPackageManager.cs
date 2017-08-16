d2acceb852cbc34316c22b921f6616b713b8dc97
Jacob Misirian <misirianrule@gmail.com>
2017-08-15 22:42:46 -0600

    Work on package manager

---
+++ src/Hassium/PackageManager/HassiumPackageManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Hassium.PackageManager
+{
+    public class HassiumPackageManager
+    {
+        private string hassiumfolder;
+
+        public HassiumPackageManager()
+        {
+            string homePath = (Environment.OSVersion.Platform == PlatformID.Unix ||
+                    Environment.OSVersion.Platform == PlatformID.MacOSX)
+                    ? Environment.GetEnvironmentVariable("HOME")
+                    : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+            hassiumfolder = Path.Combine(homePath, "/.Hassium/");
+        }
+
+        public void InstallPackage(string pkgname)
+        {
+            
+        }
+
+        public bool PackageInstalled(string pkgname)
+        {
+            return Directory.Exists(Path.Combine(hassiumfolder, pkgname));
+        }
+    }
+}
